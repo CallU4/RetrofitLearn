71990c9f65d0f50a82c3d8585ff275e116e68baf
Alex <hqwa.sm1@mail.ru>
2022-01-20 03:29:33 +0300

    Created layout and some of retrofit api stuff

---
+++ app/src/main/java/com/example/retrofitlearn/RetrofitInstance.kt
@@ -0,0 +1,10 @@
+package com.example.retrofitlearn
+
+import retrofit2.Retrofit
+
+object RetrofitInstance {
+//    val api: TodoApi by lazy{
+//        Retrofit.Builder()
+//            .baseUrl("https://jsonplaceholder.typicode.com")
+//    }
+}
